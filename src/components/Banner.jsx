be823a5359c20671e3d5624f2ec31843360dd74d
joshuatucker0914 <joshuatucker0914@gmail.com>
2023-11-16 15:52:09 -0600

    Complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import styles from "./styling.css";
+
+export default function Banner(){
+    
+    return(
+    <div>
+        <header>Orbit report</header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+    </div>
+    );
+}
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,28 @@
+import styles from "./styling.css";
 const Table = ({ sat }) => {
+  const tableBody = sat.map((data) => (
+    <tr key={data.id}>
+      <td>{data.name}</td>
+      <td>{data.type}</td>
+      <td>{data.launchDate}</td>
+      <td>{data.operational ? "Active" : "Inactive"}</td>
+    </tr>
+  ));
+
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>{tableBody}</tbody>
+    </table>
   );
 };
+
 
 export default Table;
